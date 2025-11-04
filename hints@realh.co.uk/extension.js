d59c04500f95d568d333cef3883f0ae462537a8b
Tony Houghton <h@realh.co.uk>
2025-11-04 13:43:58 +0000

    Combine Hints dbus methods into one

--- hints@realh.co.uk/extension.js
+++ hints@realh.co.uk/extension.js
@@ -12,3 +12,18 @@ export default class HintsExtension {
     disable() {
     }
 }
+
+class Hints {
+    // Returns [x, y, width, height, pid, name]
+    // If no window is focused, returns [0, 0, 0, 0, -1, ""]
+    FocusedWindowInfo() {
+        const w = global.display.get_focus_window();
+        if (!w) {
+            return [0, 0, 0, 0, -1, ""];
+        }
+        const {x, y, width, height} = w.get_frame_rect();
+        const pid = w.get_pid();
+        const name = w.get_wm_class();
+        return [x, y, width, height, pid, name];
+    }
+}
