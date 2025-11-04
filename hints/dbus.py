d494d872760f20c0851ff628715c0cbd585d78ee
Tony Houghton <h@realh.co.uk>
2025-11-04 17:10:18 +0000

    Added GDBusProxy (client)

---
+++ hints/dbus.py
@@ -0,0 +1,36 @@
+from gi.repository import Gio
+from typing import Optional
+
+class DBusHintsProxy:
+    _instance = None
+    _proxy : Optional[Gio.DBusProxy] = None
+
+    def __init__(self):
+        if DBusHintsProxy._proxy is None:
+            proxy = Gio.DBusProxy.new_for_bus_sync(
+                    bus_type = Gio.BusType.SESSION,
+                    flags = Gio.DBusProxyFlags.NONE,
+                    info = None,
+                    name = "uk.co.realh.Hints",
+                    object_path = "/uk/co/realh/Hints",
+                    interface_name = "uk.co.realh.Hints"
+                )
+            DBusHintsProxy._proxy = proxy   # type: ignore
+        else:
+            proxy = DBusHintsProxy._proxy
+        self.proxy: Gio.DBusProxy = proxy   # type: ignore
+
+    def get_focused_window_info(self) -> tuple[int, int, int, int, int, str]:
+        return self.proxy.call_sync(
+                    method_name = "FocusedWindowInfo",
+                    parameters = None,
+                    flags = Gio.DBusCallFlags.NONE,
+                    timeout_msec = -1,
+                    cancellable = None
+                )   # type: ignore
+
+    @classmethod
+    def get_instance(cls):
+        if cls._instance is None:
+            cls._instance = DBusHintsProxy()
+        return cls._instance
---
+++ hints/window_systems/gnome.py
@@ -0,0 +1,57 @@
+"""Linux window manger."""
+
+# from gi import require_version
+#
+# require_version("Wnck", "3.0")
+#
+# from gi.repository import Wnck
+
+from hints.window_systems.window_system import WindowSystem
+from hints.dbus import DBusHintsProxy
+
+
+class Gnome(WindowSystem):
+    """GNOME (on Wayland)."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        dbus_proxy = DBusHintsProxy.get_instance()
+        self.window_info: tuple[int, int, int, int, int, str] = \
+                dbus_proxy.get_focused_window_info()
+
+    @property
+    def window_system_name(self) -> str:
+        """Get the name of the window system.
+
+        This is useful for performing logic specific to a window system.
+
+        :return: The window system name
+        """
+        return "gnome"
+
+    @property
+    def focused_window_extents(self) -> tuple[int, int, int, int]:
+        """Get active window extents.
+
+        :return: Active window extents (x, y, width, height).
+        """
+        return self.window_info[:4]
+
+    @property
+    def focused_window_pid(self) -> int:
+        """Get Process ID corresponding to the focused widnow.
+
+        :return: Process ID of focused window.
+        """
+        return self.window_info[4]
+
+    @property
+    def focused_applicaiton_name(self) -> str:
+        """Get focused application name.
+
+        This name is the name used to identify applications for per-
+        application rules.
+
+        :return: Focused application name.
+        """
+        return self.window_info[5]
