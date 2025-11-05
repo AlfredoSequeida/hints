e933f7eaa6a96bf01fd817d9af3d55659e8852b6
Tony Houghton <h@realh.co.uk>
2025-11-05 13:23:10 +0000

    Enable dbus service in gnome extension

--- hints@realh.co.uk/extension.js
+++ hints@realh.co.uk/extension.js
@@ -1,15 +1,48 @@
-import St from 'gi://St';
-import Main from 'resource:///org/gnome/shell/ui/main.js';
+import Gio from 'gi://Gio';
+import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
+
+// import St from 'gi://St';
+// import Main from 'resource:///org/gnome/shell/ui/main.js';
 
 export default class HintsExtension {
     constructor() {
-        this._widget = null;
+        this._ownership = null;
+        const f = Gio.File.new_for_path(`${this.path}/uk.co.realh.Hints.xml`);
+        const [contents, _] = f.load_contents(null);
+        const decoder = new TextDecoder("utf-8");
+        this.dbusSpec = decoder.decode(contents);
     }
 
     enable() {
+        this._ownership = Gio.bus_own_name(
+            Gio.BusType.SESSION,
+            "uk.co.realh.Hints",
+            Gio.BusNameOwnerFlags.NONE,
+            this.onBusAcquired.bind(this),
+            this.onNameAcquired.bind(this),
+            this.onNameLost.bind(this),
+        );
     }
 
     disable() {
+        if (this._ownership) {
+            Gio.bus_unown_name(this._ownership);
+            this._ownership = null;
+        }
+    }
+
+    onBusAcquired(_connection, _name) {
+    }
+
+    onNameAcquired(connection, _name) {
+        const hintsService = new Hints();
+        const exportedObject = Gio.DBusExportedObject.wrapJSObject(
+            this.dbusSpec, hintsService);
+        exportedObject.export(connection, "/uk/co/realh/Hints");
+    }
+
+    onNameLost(_connection, _name) {
+        this._ownership = null;
     }
 }
 
